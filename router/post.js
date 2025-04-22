9da628474568cb1b90d0a67601c5991a8ed12f9f
GiancarloRamondino <giancarlo.ramondino@gmail.com>
2025-04-22 16:42:45 +0200

    [UPD]  fuction

--- router/post.js
+++ router/post.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();    
-const posts = require('../data/menu'); // Importa il file posts.js
+const posts = require('../data/menu.js'); // Importa il file posts.js
 
 //index
 router.get('/', function(req, res) {
@@ -9,7 +9,13 @@ router.get('/', function(req, res) {
 
 //show
 router.get('/:id', function(req, res) {
-    res.send('dettagli del menu!'+req.params.id);
+    const id = parseInt(req.params.id);
+    const post = posts.find(p => p.id === id); // cerca il post con ID
+    if (post) {
+        res.json(post);
+    } else {
+        res.status(404).send('Ricetta non trovata.');
+    }
 });
 
 //store
@@ -18,18 +24,39 @@ router.post('/', function(req, res) {
 });
 
 //update
-router.put('/', function(req, res) {
-    res.send('modifica del menu!');
+router.put('/:id', function(req, res) {
+    const id = req.params.id;
+    if (posts[id]) {
+        posts[id] = req.body; // aggiorna il post nell'array
+        res.status(200).json(posts); // restituisce l'array aggiornato
+    } else {
+        res.status(404).send('Post non trovato.');
+    }
 });
 
 //modifica
-router.patch('/', function(req, res) {
-    res.send('modifica parziale del menu!');
+router.patch('/:id', function(req, res) {
+    const id = req.params.id;
+    if (posts[id]) {
+        posts.splice(id, 1); // rimuove il post dall'array
+        posts.push(req.body); // aggiunge il nuovo post all'array
+        res.status(200).json(posts); // restituisce l'array aggiornato
+        console.log(posts); // log per il server
+    } else {
+        res.status(404).send('Post non trovato.');
+    }
 });
 
 //delete
-router.delete('/', function(req, res) {
-    res.send('cancellazione del menu!');
+router.delete('/:id', function(req, res) {
+    const id = req.params.id;
+    if (posts[id]) {
+        posts.splice(id, 1); // rimuove il post dall'array
+        res.status(200).send(``);
+        console.log(posts); // log per il server
+    } else {
+        res.status(404).send('Post non trovato.');
+    }
 });
 
 module.exports = router;
