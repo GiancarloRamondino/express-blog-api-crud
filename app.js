920ad730de42e21278ae0e99c55fa727cd2bcf7d
GiancarloRamondino <giancarlo.ramondino@gmail.com>
2025-04-28 16:31:31 +0200

    [UPD] not found and Import

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const express = require('express');
 const app = express();
 //deefinisco il numero di porta
 const port = 3000;
+//importo il middleware per la gestione degli errori
+const errorsHandler = require('./middlewares/errorsHandler.js');
+//importo il middleware per la gestione degli errori 404
+const notFound = require('./middlewares/notFound.js');
+
 
 //imposto asset statici
 app.use(express.static('imgs'));
@@ -21,6 +26,12 @@ app.use('/menu', postsRouter);
 app.get('/', (req, res) => {
   res.send('Server del mio blog!');
 });
+
+//gestisco gli errori 404
+app.use(notFound);
+
+//gestisco gli errori 500 
+app.use(errorsHandler);
 
 //server che ascolta sulla porta 3000
 app.listen(port, () => {
